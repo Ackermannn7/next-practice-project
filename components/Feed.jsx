27bdc3c4935fd2ccf7981294ffec612f6ce81049
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2024-01-31 11:38:44 +0200

    fixed revalidate

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt", { revalidate: 10 });
+    const response = await fetch("/api/prompt", { revalidate: "no-cache" });
     const data = await response.json();
 
     setAllPosts(data);
